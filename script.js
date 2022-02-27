92b6b31124407b94b5b67296f7fde8fda65f3a01
TAKANE99 <takaneleonard@gmail.com>
2022-02-27 16:18:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var menu = document.querySelector('.hamburger');
+menu = addEventListener('click' , function() {
+    document.querySelector('.nav').classList.toggle('toggle');
+})
+
+//const hamburger = document.querySelector(".hamburger");
+//const navMenu = document.querySelector(".nav-menu");
+
+//hamburger.addEventListener("click", () => {
+ //   hamburger.classList.toggle("active");
+//    navMenu.classList.toggle("active"); 
+
+//})
